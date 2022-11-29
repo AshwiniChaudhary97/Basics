4af8f497cf3afac2796db19334626c2b10ec8346
ashwinichaudhary97 <113126060+ashwinichaudhary97@users.noreply.github.com>
2022-11-30 01:29:59 +0530

    Add files via upload

--- Recursion/src/MaxIn2d.java
+++ Recursion/src/MaxIn2d.java
@@ -1,8 +1,10 @@
 public class MaxIn2d {
     public static int findMaxIn2d(int[][] array, int row){
+
         if(row == 0){
             return -1;
         }
+        /*
         int col = 0;
         int max = array[row-1][col];
 
@@ -13,12 +15,24 @@ public class MaxIn2d {
             col++;
         }
 
-        max = (max < findMaxIn2d(array, row-1)) ? findMaxIn2d(array , row-1):max;
-        return max;
+       // max = (max < findMaxIn2d(array, row-1)) ? findMaxIn2d(array , row-1):max;
+        //return max;
+
+         */
+
+        int max2 = findMaxIn2d(array, row-1);
+
+        for(int i=0; i<array[row-1].length; i++){
+            if(max2 < array[row-1][i]){
+                max2 = array[row-1][i];
+            }
+        }
+        return max2;
+
     }
 
     public static void main(String[] args) {
-        int[][] array = {{1,2,3},{2,32,4},{4,5,6}};
+        int[][] array = {{1,92,3},{2,32,4},{4,85,6}};
         System.out.println("max element in array is " + findMaxIn2d(array, array.length));
     }
 }
